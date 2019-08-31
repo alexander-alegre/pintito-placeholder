b82c3e441d33214113be5e5a2e20eb74c09db4c9
Alex Alegre <alexander.alegre@outlook.com>
2019-08-31 16:51:10 -0700

    initial commit

---
+++ teehee.js
@@ -0,0 +1,30 @@
+console.log('teehee!');
+
+const colors = [
+  [245, 66, 99],
+  [250, 213, 80],
+  [247, 250, 80],
+  [128, 250, 80],
+  [73, 217, 227],
+  [110, 0, 255],
+  [89, 58, 145],
+  [235, 52, 219]
+];
+
+function randomIntFromInterval(min, max) { // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+}
+
+function draw() {
+  let randomNumber = randomIntFromInterval(0, 7);
+  if (mouseIsPressed) {
+    fill(colors[randomNumber]);
+  } else {
+    fill(colors[randomNumber]);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+}
